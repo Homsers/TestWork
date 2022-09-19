a2d483aa9898abfcddbe3ea8ffd1d4a0311f4a58
Maxim <you@example.com>
2022-09-19 16:39:51 +0300

    program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿string[] arr1 = new string[4] {"SPb", "!", "=)", ")))))))"};
+string[] arr2 = new string[arr1.Length];
+
+void Arrs(string[] arr1, string[] arr2)
+
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+
+void Arrays(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+    Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+Arrs(arr1, arr2);
+Arrays(arr2);
